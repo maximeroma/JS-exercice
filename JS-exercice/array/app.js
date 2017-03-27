2720ff62547ba1384de76ba0273b9a3689d5e0fa
romamaxime <roma.maxime1994@gmail.com>
2017-03-27 10:25:19 +0200

    add exercice 2 : array

--- JS-exercice/array/app.js
+++ JS-exercice/array/app.js
@@ -1,2 +1,17 @@
 var array = ['Pomme','Cerise','Tomate','Citron', ['Orange','Poivron']];
+
+/*	##Exercice  1 */
+
 var mois = ['janvier', 'fevrier', 'mars', 'avril', 'mai', 'juin', 'juillet', 'aout', 'septembre', 'octobre', 'novembre', 'decembre'];
+console.log(mois);
+
+/*	##Exercice 2 */
+
+console.log(mois[2]);
+
+/*	##Exercice 3 */
+
+
+
+
+
