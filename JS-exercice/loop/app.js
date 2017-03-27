51ac8a10989df946ebc0711577eaaf936f92036d
romamaxime <roma.maxime1994@gmail.com>
2017-03-27 14:48:25 +0200

    add loop: exo2

--- JS-exercice/loop/app.js
+++ JS-exercice/loop/app.js
@@ -1,5 +1,15 @@
 	/* Exercice1 */
 
 for (var i = 0 ; i <= 10; i++){
-	console.log(i);
+	//console.log(i);
 }
+
+	/*Exercice2 */
+
+var multipleInLoop = function(a, b){
+	for( a; a < 21; a++){
+		console.log(a * b);
+	}
+};
+
+multipleInLoop(3, 2);
