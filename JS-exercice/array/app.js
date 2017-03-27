8b36c84451d674b1047998ec3aad6b48866cac3b
romamaxime <roma.maxime1994@gmail.com>
2017-03-27 10:17:34 +0200

    add mois' array

---
+++ JS-exercice/array/app.js
@@ -0,0 +1,2 @@
+var array = ['Pomme','Cerise','Tomate','Citron', ['Orange','Poivron']];
+var mois = ['janvier', 'fevrier', 'mars', 'avril', 'mai', 'juin', 'juillet', 'aout', 'septembre', 'octobre', 'novembre', 'decembre'];
