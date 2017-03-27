283724aa62c7f0d87f77930b91e297feea8a51c8
romamaxime <roma.maxime1994@gmail.com>
2017-03-27 11:44:04 +0200

    add functions: exo4

--- JS-exercice/functions/app.js
+++ JS-exercice/functions/app.js
@@ -22,3 +22,16 @@ var concatString = function(a, b) {
 
 console.log(concatString('Hello', 'World!'));
 
+	/* Exercice4 */
+
+var compareNumber = function(a, b) {
+	if ( a === b){
+		return "Les deux nombres sont identiques";
+	} else if( a < b){
+		return "Le premier nombre est plus petit";
+	} else {
+		return "Le premier nombre est plus grand";
+	}
+}
+
+console.log(compareNumber(8, 6));
